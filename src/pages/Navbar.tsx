ebbe0e185270a0a7fb434fbb5025cd54bf37df24
Antonin Lmp <73228586+AntoninLmp@users.noreply.github.com>
2025-06-21 11:45:10 +0200

    <feat>: Add scroll effect to Navbar

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,26 +1,45 @@
+import { useEffect, useState } from "react";
+
 function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 60);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <nav className="navbar flex justify-around items-center p-4">
       <a href="/">
-        <img src="/logo-removebg-resize.png" alt="Logo" className="navbar__logo-img w-[150px]" />
+        <img src="/logo-removebg-resize.png" alt="Logo" className="w-[150px]" />
       </a>
-      <ul className="navbar__links flex space-x-4 text-xl gap-8 text-gray-800 fixed" >
-        <li>
-          <a href="/">Acceuil</a>
-        </li>
-        <li>
-          <a href="/">Services</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-        <li>
-          <a href="/about">A propos</a>
-        </li>
-      </ul>
+      <div className="flex justify-center">
+        <ul
+          className={`flex space-x-4 gap-8 text-gray-800 text-l transition-transform duration-1000  ${
+            scrolled ? "translate-y-5 fixed top-0.5 bg-gray-100 p-5 rounded-2xl" : "translate-y-0"
+          }`}
+        >
+          <li>
+            <a href="/">Acceuil</a>
+          </li>
+          <li>
+            <a href="/">Services</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <a href="/about">A propos</a>
+          </li>
+        </ul>
+      </div>
       <button className="bg-blue-500 text-white">
         <a href="/login" className="btn">
-          Login
+          Faire un devis
         </a>
       </button>
     </nav>
